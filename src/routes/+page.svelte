dd98e3080b4a575085f611098261a906e6c4cfc2
Alvin Meroy <archnivs@gmail.com>
2023-07-01 06:49:19 +0800

    basic page layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Aside from './components/aside.svelte';
+    import Content from './components/content.svelte';
+</script>
+
+<div id="wrap">
+    <div class="container">
+        <div class="aside">
+            <Aside />
+        </div>
+        <div class="content">
+            <Content />
+        </div>
+    </div>
+</div>
+
+<style>
+    .aside,
+    .content {
+        width: 100%;
+        padding: 1rem;
+        flex-grow: 1;
+        flex-wrap: wrap;
+    }
+
+    @media screen and (min-width: 768px) {
+        .aside {
+            width: 40%;
+        }
+        .content {
+            width: 60%;
+        }
+    }
+</style>
