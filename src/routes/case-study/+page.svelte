7f2d09cee3973f052be2c9236517592bc02a6c58
Alvin Meroy <archnivs@gmail.com>
2023-07-14 21:06:43 +0800

    Case Study route

---
+++ src/routes/case-study/+page.svelte
@@ -0,0 +1,34 @@
+
+
+<div id="wrap">
+    <div class="container">
+        <div class="aside">
+            <h1>Case Study</h1>
+        </div>
+        <div class="content">
+            <p>TODO...</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .aside,
+    .content {
+        width: 100%;
+        padding: 0;
+        flex-grow: 1;
+        flex-wrap: wrap;
+    }
+
+    @media screen and (min-width: 768px) {
+        #wrap {
+            margin-top: 4em;
+        }
+        .aside {
+            width: 33%;
+        }
+        .content {
+            width: 66%;
+        }
+    }
+</style>
