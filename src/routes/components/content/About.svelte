f0c67ba9f3e289edb1b3a896d1a9a7c8d27251a4
Alvin Meroy <archnivs@gmail.com>
2023-07-18 20:13:23 +0800

    ---

--- src/routes/components/content/About.svelte
+++ src/routes/components/content/About.svelte
@@ -1,7 +1,7 @@
 <section id="about" class="section-wrapper section-about">
-    <p>Hi! I am Alvin Meroy. I have been working as a web developer for more than 10 years now. Over the years, I've had the pleasure of working with a wide range of businesses, from Marketing agency, an online school for writers to a web development team. Most of the project I am involve with are develop using WordPress, either transform a mockup to a custom WordPress Theme or to an E-commerce.</p>
+    <p>Hi! I am Alvin Meroy. I have been employed as a web developer for over a decade now. Throughout my career, I have had the opportunity to collaborate with various types of businesses, including a marketing agency, an online school for writers, and a web development team. The majority of the projects I am involved in are implemented using WordPress. This involves either transforming a mockup into a custom WordPress theme or developing an e-commerce solution.</p>
 
-    <p>The first two agency I work with is Englund Studio from Rhode Island and WIDSIX from Arizona. Both are from USA. In Englund Studio, the usual project that I do are for small businesses residing Rhode Island while in WIDSIX I create a custom WordPress theme for branding an Extreme Sport Athlete. </p>
+    <p>During my early experiences, I worked with Englund Studio based in Rhode Island and WIDSIX located in Arizona, both being agencies from the United States. At Englund Studio, I primarily focused on projects for small businesses in Rhode Island, whereas at WIDSIX, I created a custom WordPress theme to establish the brand presence of an extreme sport athlete. </p>
 
-    <p>My approach in a project usually start of what is the purpose of the project and how I can developed it and meet the requirement. Break it down to a small tasks and start coding from there.</p>
+    <p>When approaching a project, my methodology typically begins by understanding its purpose and determining the best way to develop and fulfill the requirements. I break down the project into smaller tasks and commence coding from there.</p>
 </section>
